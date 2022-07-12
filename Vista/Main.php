a1ac00e67604b92a37b2bdb542a6784e91e6e0e4
FauricioCespedes <fauriciocespedes@gmail.com>
2022-07-11 19:59:14 -0600

    responsive1

--- Vista/Main.php
+++ Vista/Main.php
@@ -8,31 +8,26 @@
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
 		<!--JavaScript-->
 		<script src="./Vista/js/scripts.js" defer></script>
+		<!--font awesome-->
+		<script src="https://kit.fontawesome.com/29735a6008.js" crossorigin="anonymous"></script>
 
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+		<script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 		<link rel="stylesheet" href="./Vista/estilos.css">
     <title>Feria Virtual</title>
   </head>
   	<body class="mh-100-vh">
-				<header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between mb-4 border-bottom">
-					<a href="/" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
-						<img width="110" height="50" src="https://admision.covao.org/Grafica/image/logo.png" class="img-fluid" >
-					</a>
+				<header class="px-2 d-flex flex-wrap align-items-center justify-content-between border-bottom">
+					<img width="110" height="50" src="https://admision.covao.org/Grafica/image/logo.png" class="img-fluid" >
 				
-					<ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
-						<li><a href="index.html" class="nav-link px-2 link-secondary">Inicio</a></li>
-						<li><a href="#" class="nav-link px-2 link-dark">Especialidades</a></li>
-					</ul>
-				
-					<div class="col-md-3 text-end">
+		
 						<a href="index.php?controlador=Index&accion=Registro">
 							<button type="button" class="btn btn-outline-primary me-2">Registro</button>
 						</a>
-					</div>
 				</header>	
-				<main>
+				<main class="py-4">
 						<div class="map-container">
 								<img class="map-container__image" src="./Vista/assets/images/mapa.png" alt="Mapa COVAO">
 
@@ -46,10 +41,8 @@
 								<div class="point point-automotriz animate__animated animate__heartBeat animate__infinite"></div>
 								<div class="point point-secretariado animate__animated animate__heartBeat animate__infinite"></div>
 						</div>
-
-<<<<<<< HEAD
 				</main>
-				<footer class="fixed-bottom py-2">
+				<footer class="postion-sticky bottom-0 w-100 py-3 ps-4 border-top">
 					<div class="mx-auto" style="width: 200px;">
 						<span class="text-muted">© 2022 Feria Virtual Covao</span>
 						<ul class="nav	">
@@ -60,30 +53,6 @@
 						</ul>
 					</div>
 				</footer>
-			</body>
-			<script src="https://kit.fontawesome.com/29735a6008.js" crossorigin="anonymous"></script>
-			<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
   </body>
 </html>
-=======
-		<main>
-
-
-		</main>
 	
-		<footer class="postion-sticky bottom-0 w-100 py-3 ps-4 border-top">
-			<div class="mx-auto" style="width: 200px;">
-				<span class="text-muted">© 2022 Feria Virtual Covao</span>
-				<ul class="nav	">
-						<li class="ms-3"><a class="text-muted" href="https://www.instagram.com/covao.diurno/?hl=es-la"><svg class="bi" width="24" height="24" viewBox="0 0 448 512"><path d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"/></svg></a></li>
-						<li class="ms-3"><a class="text-muted" href="https://www.youtube.com/channel/UCLfrfTSgNqIrkKvYmNtwODg"><svg class="bi" width="24" height="24" viewBox="0 0 576 512"><path d="M549.655 124.083c-6.281-23.65-24.787-42.276-48.284-48.597C458.781 64 288 64 288 64S117.22 64 74.629 75.486c-23.497 6.322-42.003 24.947-48.284 48.597-11.412 42.867-11.412 132.305-11.412 132.305s0 89.438 11.412 132.305c6.281 23.65 24.787 41.5 48.284 47.821C117.22 448 288 448 288 448s170.78 0 213.371-11.486c23.497-6.321 42.003-24.171 48.284-47.821 11.412-42.867 11.412-132.305 11.412-132.305s0-89.438-11.412-132.305zm-317.51 213.508V175.185l142.739 81.205-142.739 81.201z"/></svg></a></li>
-						<li class="ms-3"><a class="text-muted" href="https://www.facebook.com/covaodiurno"><svg class="bi" width="24" height="24" viewBox="0 0 512 512"><path d="M504 256C504 119 393 8 256 8S8 119 8 256c0 123.78 90.69 226.38 209.25 245V327.69h-63V256h63v-54.64c0-62.15 37-96.48 93.67-96.48 27.14 0 55.52 4.84 55.52 4.84v61h-31.28c-30.8 0-40.41 19.12-40.41 38.73V256h68.78l-11 71.69h-57.78V501C413.31 482.38 504 379.78 504 256z"/></svg></a></li>
-						<li class="ms-3"><a class="text-muted" href="https://covao.org/"><svg class="bi" width="24" height="24" viewBox="0 0 512 512"><path d="M448 32C483.3 32 512 60.65 512 96V416C512 451.3 483.3 480 448 480H64C28.65 480 0 451.3 0 416V96C0 60.65 28.65 32 64 32H448zM96 96C78.33 96 64 110.3 64 128C64 145.7 78.33 160 96 160H416C433.7 160 448 145.7 448 128C448 110.3 433.7 96 416 96H96z"/></svg></a></li>
-				</ul>
-			</div>
-		</footer>
-		<script src="https://kit.fontawesome.com/29735a6008.js" crossorigin="anonymous"></script>
-		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-	</body>
-</html>
->>>>>>> b5140c2d662d1b2d07ad099f7d64d5bae5889435

989c58b1d1810c4eabfc95473afce06b0964b0e7
FauricioCespedes <fauriciocespedes@gmail.com>
2022-07-12 10:44:59 -0600

    responsive2

--- Vista/Main.php
+++ Vista/Main.php
@@ -10,6 +10,15 @@
 		<script src="./Vista/js/scripts.js" defer></script>
 		<!--font awesome-->
 		<script src="https://kit.fontawesome.com/29735a6008.js" crossorigin="anonymous"></script>
+		<!--favicon-->
+		<link rel="apple-touch-icon" sizes="180x180" href="./Vista/assets/favicon/apple-touch-icon.png">
+		<link rel="icon" type="image/png" sizes="32x32" href="./Vista/assets/favicon/favicon-32x32.png">
+		<link rel="icon" type="image/png" sizes="16x16" href="./Vista/assets/favicon/favicon-16x16.png">
+		<link rel="manifest" href="./Vista/assets/favicon/site.webmanifest">
+		<!--font-->
+		<link rel="preconnect" href="https://fonts.googleapis.com">
+		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+		<link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@400;700&display=swap" rel="stylesheet"> 
 
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -19,15 +28,18 @@
     <title>Feria Virtual</title>
   </head>
   	<body class="mh-100-vh">
-				<header class="px-2 d-flex flex-wrap align-items-center justify-content-between border-bottom">
+				<header class="px-5 d-flex flex-wrap align-items-center justify-content-between general-shadow">
 					<img width="110" height="50" src="https://admision.covao.org/Grafica/image/logo.png" class="img-fluid" >
-				
-		
-						<a href="index.php?controlador=Index&accion=Registro">
-							<button type="button" class="btn btn-outline-primary me-2">Registro</button>
-						</a>
+					<a href="index.php?controlador=Index&accion=Registro">
+						<button type="button" class="btn btn-outline-primary me-2">Registrarme</button>
+					</a>
 				</header>	
-				<main class="py-4">
+				<main class="main-container py-4">
+						<div class="texto-container px-4" style="font-size: 1.2rem;">
+							<h2 class="txtregistro fs-3 mt-2 mb-3">Bienvenido a la Feria Virtual COVAO 2022</h2>
+							<p>Aquí podrá conocer más sobre todas las especialidades impartidas, ubicadas en el mapa del colegio según su ubicación real mediante los puntos blancos.</p>
+							<p>En caso de que quiera recibir más información puede registrar su correo electrónico en la parte superior derecha de la página.</p>	
+						</div>
 						<div class="map-container">
 								<img class="map-container__image" src="./Vista/assets/images/mapa.png" alt="Mapa COVAO">
 
@@ -42,15 +54,31 @@
 								<div class="point point-secretariado animate__animated animate__heartBeat animate__infinite"></div>
 						</div>
 				</main>
-				<footer class="postion-sticky bottom-0 w-100 py-3 ps-4 border-top">
-					<div class="mx-auto" style="width: 200px;">
-						<span class="text-muted">© 2022 Feria Virtual Covao</span>
-						<ul class="nav	">
-								<li class="ms-3"><a class="text-muted" href="https://www.instagram.com/covao.diurno/?hl=es-la"><svg class="bi" width="24" height="24" viewBox="0 0 448 512"><path d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"/></svg></a></li>
-								<li class="ms-3"><a class="text-muted" href="https://www.youtube.com/channel/UCLfrfTSgNqIrkKvYmNtwODg"><svg class="bi" width="24" height="24" viewBox="0 0 576 512"><path d="M549.655 124.083c-6.281-23.65-24.787-42.276-48.284-48.597C458.781 64 288 64 288 64S117.22 64 74.629 75.486c-23.497 6.322-42.003 24.947-48.284 48.597-11.412 42.867-11.412 132.305-11.412 132.305s0 89.438 11.412 132.305c6.281 23.65 24.787 41.5 48.284 47.821C117.22 448 288 448 288 448s170.78 0 213.371-11.486c23.497-6.321 42.003-24.171 48.284-47.821 11.412-42.867 11.412-132.305 11.412-132.305s0-89.438-11.412-132.305zm-317.51 213.508V175.185l142.739 81.205-142.739 81.201z"/></svg></a></li>
-								<li class="ms-3"><a class="text-muted" href="https://www.facebook.com/covaodiurno"><svg class="bi" width="24" height="24" viewBox="0 0 512 512"><path d="M504 256C504 119 393 8 256 8S8 119 8 256c0 123.78 90.69 226.38 209.25 245V327.69h-63V256h63v-54.64c0-62.15 37-96.48 93.67-96.48 27.14 0 55.52 4.84 55.52 4.84v61h-31.28c-30.8 0-40.41 19.12-40.41 38.73V256h68.78l-11 71.69h-57.78V501C413.31 482.38 504 379.78 504 256z"/></svg></a></li>
-								<li class="ms-3"><a class="text-muted" href="https://covao.org/"><svg class="bi" width="24" height="24" viewBox="0 0 512 512"><path d="M448 32C483.3 32 512 60.65 512 96V416C512 451.3 483.3 480 448 480H64C28.65 480 0 451.3 0 416V96C0 60.65 28.65 32 64 32H448zM96 96C78.33 96 64 110.3 64 128C64 145.7 78.33 160 96 160H416C433.7 160 448 145.7 448 128C448 110.3 433.7 96 416 96H96z"/></svg></a></li>
+				<footer class="postion-sticky bottom-0 w-100 py-3 px-5 general-shadow">
+					<div class="d-flex justify-content-between">
+						<ul class="nav gap-3" style="font-size: 1.2rem;">
+							 <li class="">
+									<a class="text-muted" href="https://www.instagram.com/covao.diurno/?hl=es-la">
+										<i class="fa-brands fa-instagram"></i>
+									</a>
+							 </li>
+							 <li class="">
+									<a class="text-muted" href="https://www.youtube.com/channel/UCLfrfTSgNqIrkKvYmNtwODg">
+										<i class="fa-brands fa-youtube"></i>
+									</a>
+							</li>
+							 <li class="">
+									<a class="text-muted" href="https://www.facebook.com/covaodiurno">
+										<i class="fa-brands fa-facebook-square"></i>
+									</a>
+							</li>
+							 <li class="">
+									<a class="text-muted" href="https://covao.org/">
+										<i class="fa-solid fa-globe"></i>
+									</a>
+							</li>
 						</ul>
+						<span class="text-muted">© 2022 Feria Virtual Covao</span>
 					</div>
 				</footer>
   </body>
